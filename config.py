4e7c25df6f286c1b1ad3f0c845285032edd18a2d
stricte <stricte@gmail.com>
2026-04-05 17:06:57 +0200

    Add HTTP ingress skeleton for Volumio notifications

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ class AppConfig:
         self.broker_ip = "127.0.0.1"
         self.broker_port = 1883
         self.broker_topic = "nad"
+        self.mqtt_ingress_enabled = True
+        self.http_ingress_enabled = False
+        self.http_ingress_shadow_mode = True
+        self.http_ingress_host = "127.0.0.1"
+        self.http_ingress_port = 8080
+        self.http_ingress_path = "/ingress/volumio/notifications"
         self.logger_name = "mqtt_receive"
         self.logger_path = "/var/log/nad/mqtt_receive.log"
         self.serial = "/dev/ttyUSB0"
